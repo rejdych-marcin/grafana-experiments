f5e321a913fce20636c790751723f8b9c1b4a660
mrejdych <rejdych.marcin.1@gmail.com>
2022-03-09 22:29:20 +0100

    dummy microservice with metrics

---
+++ dummy-microservice/src/main/java/app/ApiAppEntry.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppEntry {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAppEntry.class, args);
+    }
+}
